580dd9cb9ea1212cc641d4b7e45c4fc66371f875
TwardyS <filiptwardowski@onetpl>
2025-03-11 10:11:10 +0100

    Dodaj pliki projektów.

---
+++ zadanie egzaminacyjne/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace zadanie_egzaminacyjne
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
